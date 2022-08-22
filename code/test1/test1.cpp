47ed96ba62c5cebaa4587a74e19434f914d7c2f0
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2022-08-22 11:53:39 +0100

    midi working in test file

--- code/test1/test1.cpp
+++ code/test1/test1.cpp
@@ -6,7 +6,7 @@ using namespace daisy;
 using namespace daisysp;
 
 DaisySeed hw;
-TestLib testLib;
+MidiUartHandler midi;
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
@@ -17,13 +17,52 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, s
 	}
 }
 
+// Typical Switch case for Message Type.
+void HandleMidiMessage(MidiEvent m)
+{
+	switch (m.type)
+	{
+	case NoteOn:
+	{
+		hw.PrintLine("note on");
+		NoteOnEvent p = m.AsNoteOn();
+		// This is to avoid Max/MSP Note outs for now..
+		if (m.data[1] != 0)
+		{
+			p = m.AsNoteOn();
+			hw.PrintLine("note: %d", p.note);
+		}
+	}
+	break;
+	default:
+		hw.PrintLine("something else");
+		break;
+	}
+}
+
 int main(void)
 {
 	hw.Init();
 	hw.SetAudioBlockSize(4); // number of samples handled per callback
 	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
 	hw.StartAudio(AudioCallback);
+
 	hw.StartLog(true);
-	hw.PrintLine("this is a test %d", testLib.myNum);
-	while(1) {}
+	hw.PrintLine("Attempting MIDI input...");
+
+	MidiUartHandler::Config midiConfig;
+	midiConfig.transport_config.rx = daisy::seed::D14;
+	midiConfig.transport_config.tx = daisy::seed::D13;
+	midi.Init(midiConfig);
+	midi.StartReceive();
+
+	for (;;)
+	{
+		midi.Listen();
+		// Handle MIDI Events
+		while (midi.HasEvents())
+		{
+			HandleMidiMessage(midi.PopEvent());
+		}
+	}
 }
